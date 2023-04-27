aee1bd28f0552b5cc10e414ac099e5a58e461d36
Carlos Fabres <ca.fabres@duocuc.cl>
2023-04-27 18:46:33 -0400

    Register

--- Proyecto_titulo/Gaminghub/urls.py
+++ Proyecto_titulo/Gaminghub/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login # Se importa la vista de urls
+from .views import login, register # Se importa la vista de urls
 
 urlpatterns = [
     path('', login, name='login'),
+    path('register', register, name='register'),
    
 ]
