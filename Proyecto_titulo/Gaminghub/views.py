4d629d401e0d2f7320d936499b53bf727966e865
DESKTOP-GTU0A92\HP 840 G4 <vicente444.es@gmail.com>
2023-04-27 18:23:41 -0400

    Perfil

--- Proyecto_titulo/Gaminghub/views.py
+++ Proyecto_titulo/Gaminghub/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def login(request):
     return render(request,'login.html')
+
+def perfil(request):
+    return render(request,'perfil.html')
