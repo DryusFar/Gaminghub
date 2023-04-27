baa79e158e1bb61fe29f60baf72fd136cdb17482
DESKTOP-GTU0A92\HP 840 G4 <vicente444.es@gmail.com>
2023-04-27 18:52:56 -0400

    Perfill

--- Proyecto_titulo/Gaminghub/urls.py
+++ Proyecto_titulo/Gaminghub/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import login # Se importa la vista de urls
+from .views import login, perfil # Se importa la vista de urls
 
 urlpatterns = [
     path('', login, name='login'),
-   
+    path('perfil/', perfil, name='perfil')
 ]
