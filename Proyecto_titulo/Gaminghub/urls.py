0f2c4147c21a5e4a39383dbbdaee8810a614649f
Kviinalx <ke.oyaneder@duocuc.cl>
2023-05-06 15:44:37 -0400

    menu principal

--- Proyecto_titulo/Gaminghub/urls.py
+++ Proyecto_titulo/Gaminghub/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login, perfil # Se importa la vista de urls
+from .views import login, perfil ,index,register# Se importa la vista de urls
 
 urlpatterns = [
-    path('', login, name='login'),
+    path('login', login, name='login'),
     path('register', register, name='register'),
-path('perfil/', perfil, name='perfil')
+path('perfil/', perfil, name='perfil'),
 path('', index, name='index'),
 ]
