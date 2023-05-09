f87ea226a86b5bbc4109e973de3794c715e4984b
Carlos Fabres <ca.fabres@duocuc.cl>
2023-05-09 18:43:31 -0400

    formularios django session

--- Proyecto_titulo/Gaminghub/views.py
+++ Proyecto_titulo/Gaminghub/views.py
@@ -1,30 +1,110 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 # Create your views here.
-def login(request):
-    return render(request,'login.html')
+
+
+def loginView(request):
+    return render(request, 'loginView.html')
+
 
 def perfil(request):
-    return render(request,'perfil.html')
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        username = None
+
+    context = {
+        'username': username
+    }
+    return render(request, 'perfil.html',context)
+
 
 def admin1(request):
-    return render(request,'admin1.html')
+    return render(request, 'admin1.html')
+
 
 def chat(request):
-    return render(request,'chat.html')
+    return render(request, 'chat.html')
+
 
 def menu_principal(request):
-    return render(request,'menu_principal.html')
+    return render(request, 'menu_principal.html')
+
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 def register(request):
-    return render(request,'register.html')
+    return render(request, 'register.html')
+
 
 def form_publicacion(request):
-    return render(request,'form_publicacion.html')
+    return render(request, 'form_publicacion.html')
+
 
 def completar_perfil(request):
-    return render(request,'completar_perfil.html')
+    return render(request, 'completar_perfil.html')
+
+
+# REGISTRARSE
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('index')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    "error": 'Usuario ya existe'
+                })
+
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Las contraseñas no coinciden'
+        })
+
+
+# CERRAR SESIÓN
+def signout(request):
+    logout(request)
+    return redirect('signin')
+
+
+
+# INICIAR SESIÓN
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario y/o contraseña incorrecto'
+            })
+        else:
+            login(request, user)
+            return redirect('index')
+
+
